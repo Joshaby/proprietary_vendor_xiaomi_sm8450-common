999e4937d5a31afa4596696f25cca77e38833e4f
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-04-10 11:30:21 -0300

    vendor-sm8450-common: Add new gpudrivers apk and fix build

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -779,7 +779,7 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.taro.api31 \
+    com.qualcomm.qti.gpudrivers.pineapple.api34 \
     uimgbaservice \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentYGoogleHEXAGON_WIDEBAND \
